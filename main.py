6c0e73be54a60d3008b968932c473fdc2db70633
smhyaci <seanhyacinthe@gmail.com>
2022-07-14 23:25:10 -0700

    Added in UI for data entry panel, but not functional

--- main.py
+++ main.py
@@ -2,10 +2,16 @@
 # TODO: clean up import statements
 from kivy.app import App
 from TransactionEntryPanel import TransactionEntryPanel
+from kivy.core.window import Window
 
 
+main_window_bg_color = (190/255, 190/255 , 190/255, 1)
+
+# creates the main window view
 class FinanceApp(App):
     def build(self):
+        #set color of background window
+        Window.clearcolor =  main_window_bg_color
         return TransactionEntryPanel()  
     
     
