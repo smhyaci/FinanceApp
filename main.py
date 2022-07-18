5736abe38fb6ee441af6f3488fbd360ad6c897f4
smhyaci <seanhyacinthe@gmail.com>
2022-07-17 23:04:00 -0700

    GUI Revamp
    Added KivyMD library to make GUI more modern. Renamed Entry Panel Class

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 
 # TODO: clean up import statements
-from kivy.app import App
-from TransactionEntryPanel import TransactionEntryPanel
-from kivy.core.window import Window
+from kivymd.app import MDApp
+from DataEntryPanel import DataEntryPanel
 
 
-main_window_bg_color = (190/255, 190/255 , 190/255, 1)
 
 # creates the main window view
-class FinanceApp(App):
+class FinanceApp(MDApp):
     def build(self):
-        #set color of background window
-        Window.clearcolor =  main_window_bg_color
-        return TransactionEntryPanel()  
+        
+        #set color scheme for app
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "Orange"
+        
+        return DataEntryPanel()  
     
     
 if __name__ == "__main__":
